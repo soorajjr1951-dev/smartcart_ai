d9c487cef40c4d6cc2a5eedaa77c14479f50ef06
soorajjr1951-dev <soorajjr1951@gmail.com>
2026-01-21 10:57:42 +0530

    api Service

---
+++ smartcart_frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home() {
+    return (
+        <div className="home-page">
+            <div className="home-featured">
+                
+            </div>
+        </div>
+    );
+}
+
+export default Home;
